7c9fa0624e339916786e006b8a214db433dbbc14
Viktor Bocz <beviktor95@gmail.com>
2020-09-21 15:19:41 +0200

    Init project with micro-services

---
+++ detailed-beer-service/src/main/java/com/codecool/detailedbeerservice/DetailedBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.detailedbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetailedBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetailedBeerServiceApplication.class, args);
+	}
+
+}
---
+++ detailed-beer-service/src/test/java/com/codecool/detailedbeerservice/DetailedBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.detailedbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetailedBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
