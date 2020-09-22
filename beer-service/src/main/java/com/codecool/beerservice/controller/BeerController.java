ef38ae677cdd2422e9af7ad2e6067d90de26bab4
Viktor Bocz <beviktor95@gmail.com>
2020-09-22 11:54:04 +0200

    Endpoints for all beers and random beer

---
+++ beer-service/src/main/java/com/codecool/beerservice/controller/BeerController.java
@@ -0,0 +1,29 @@
+package com.codecool.beerservice.controller;
+
+import com.codecool.beerservice.component.RemoteURLReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/beer")
+public class BeerController {
+
+    private final String apiBasicEndpoint = "https://api.punkapi.com/v2/beers";
+
+    @Autowired
+    RemoteURLReader remoteURLReader;
+
+
+    @GetMapping("/all/{page}")
+    public String returnBeers(@PathVariable("page") String page) throws IOException {
+        return remoteURLReader.readFromUrl(apiBasicEndpoint + "?page=" + page);
+    }
+
+    @GetMapping("/random")
+    public String returnRandomBeer() throws IOException {
+        return remoteURLReader.readFromUrl(apiBasicEndpoint + "/random");
+    }
+}
