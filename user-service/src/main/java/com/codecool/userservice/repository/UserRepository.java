9d84326eb9b80bb9c14f8c6452aef466216b014f
laczkobalazs <laczko.balazs89@gmail.com>
2020-09-23 17:21:58 +0200

    adds userRepository

--- user-service/src/main/java/com/codecool/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/codecool/userservice/repository/UserRepository.java
@@ -3,5 +3,11 @@ package com.codecool.userservice.repository;
 import com.codecool.userservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserName(String username);
+
+    Optional<User> deleteUserById(Long id);
 }
