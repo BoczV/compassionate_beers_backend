a7696af642858255d465761b379c0b6bedb08453
laczkobalazs <laczko.balazs89@gmail.com>
2020-09-24 18:18:29 +0200

    adds security package to userservice

--- user-service/src/main/java/com/codecool/userservice/component/FindUserByCookie.java
+++ user-service/src/main/java/com/codecool/userservice/component/FindUserByCookie.java
@@ -3,6 +3,8 @@ package com.codecool.userservice.component;
 import com.codecool.userservice.model.User;
 import com.codecool.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,7 +13,9 @@ public class FindUserByCookie {
     @Autowired
     private UserRepository userRepository;
 
-    public User findUser() {
-
+    public User findUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = (String) authentication.getPrincipal();
+        return userRepository.findByUserName(userName).get();
     }
 }
